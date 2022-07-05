a2234f787638c02af33ebf1c65609c6f0f49f056
Erik Steiger <rkstgr@gmail.com>
2022-07-05 14:47:10 +0200

    Update verify only check tracks specified in --path

--- verify.py
+++ verify.py
@@ -16,14 +16,27 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--dir', type=Path, required=True)
     parser.add_argument('--skip', type=int, default=0)
+    parser.add_argument('--path', type=Path, required=False, default=None)
     args = parser.parse_args()
     directory = Path(args.dir)
     skip = args.skip
 
     tracks = createDataset.load_tracks()
-    tracks = tracks[skip:]
     print("Verifying dataset in", directory.absolute())
-    print("Skipping the first", skip, "tracks")
+
+    if args.path is not None:
+        print("Verifying tracks from", args.path.absolute())
+        path = args.path
+        lines = open("missing.txt").readlines()
+        ids = set()
+        for line in lines:
+            ids.add(int(line.split("/")[1].split(".")[0]))
+        tracks = [t for t in tracks if t.id in ids]
+
+    if skip > 0:
+        print("Skipping the first", skip, "tracks")
+
+    tracks = tracks[skip:]
 
     missing = []
     wrong_sha256 = []
