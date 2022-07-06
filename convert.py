7733140df47cefd4032638b423b5dd40efdd6c34
Erik Steiger <rkstgr@gmail.com>
2022-07-06 14:19:42 +0200

    Use ffmpeg-python

--- convert.py
+++ convert.py
@@ -6,22 +6,24 @@ from pathlib import Path
 import argparse
 import subprocess
 from typing import Tuple, List
-
+import ffmpeg
 import parallelbar
 import tqdm
 from parallelbar import progress_map
 
 
-def convert(tasks: List[Tuple[Path, Path, Path]]):
-    from_mp3, to_opus, ffmpeg_path = tasks
+def convert(tasks: List[Tuple[Path, Path, int]]):
+    from_mp3, to_opus, kBits = tasks
     if to_opus.exists():
         print("Skipping", to_opus.absolute(), "(already exists)")
         return
 
-    subprocess.run(
-        [ffmpeg_path, '-y', '-i', from_mp3, '-c:a', 'libopus',
-         '-b:a',
-         '64k', to_opus], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    (
+        ffmpeg
+        .input(str(from_mp3.absolute()))
+        .output(str(to_opus.absolute()), acodec="libopus", audio_bitrate=f"{kBits}k")
+        .run(overwrite_output=True)
+    )
 
     if not to_opus.exists():
         print("Conversion failed for ", from_mp3.absolute())
@@ -32,14 +34,13 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Convert mp3 files to opus files")
     parser.add_argument("--from", help="Directory containing mp3 files", required=True)
     parser.add_argument("--to", help="Directory to write opus files to", required=True)
-    parser.add_argument("--ffmpeg", help="Path to ffmpeg", default="ffmpeg")
+    #parser.add_argument("--ffmpeg", help="Path to ffmpeg", default="ffmpeg")
     parser.add_argument("--kbits", default=64, type=int, help="Bitrate of opus file")
     parser.add_argument("--n_cpu", default=1, type=int, help="Number of CPUs to use")
     args = parser.parse_args()
 
     mp3_directory = Path(args.__getattribute__("from"))
     target_directory = Path(args.to)
-    ffmpeg_path = Path(args.ffmpeg)
 
     if not mp3_directory.exists():
         raise FileNotFoundError(f"Directory {mp3_directory} does not exist")
@@ -50,7 +51,7 @@ if __name__ == '__main__':
 
     mp3_files = list(mp3_directory.glob("**/*.mp3"))
     opus_files = [target_directory / f"{f.stem}.opus" for f in mp3_files]
-    tasks = list(zip(mp3_files, opus_files, repeat(ffmpeg_path)))
+    tasks = list(zip(mp3_files, opus_files, repeat(args.kbits)))
 
     print("Converting", len(tasks), "files")
 
